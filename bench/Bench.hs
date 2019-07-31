6bda4f1f80d2a5482f6794859cd5714567d2aa32
Javran Cheng <Javran.c@gmail.com>
2019-07-30 23:15:11 -0700

    keep it simple for now.

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -6,19 +6,12 @@ import Criterion.Main
 
 import Petbox
 
-setupEnv :: IO ([Int], [Integer])
-setupEnv = do
-  let small0 = [1..100000]
-      small1 = [100000..200000]
-  pure (small0, small1)
-
 main :: IO ()
 main = defaultMain
-  [ env setupEnv $ \ ~(small0,small1) ->
-    bgroup "digitLen"
-      [ bench "small Int" $
-          nf (fmap digitLen) small0
+  [ bgroup "digitLen"
+      [ bench "maxBound Int" $
+          nf digitLen (maxBound :: Int)
       , bench "small Integer" $
-          nf (fmap digitLen) small1
+          nf digitLen (2 ^! 1000 :: Integer)
       ]
   ]
